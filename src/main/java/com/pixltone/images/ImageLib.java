1e7da11aeddc52e1caae292aeb573330627b1169
GoodiesHQ <goodies@protonmail.com>
2016-04-10 00:24:28 -0700

    Initial Commit

---
+++ src/main/java/com/pixltone/images/ImageLib.java
@@ -0,0 +1,12 @@
+package com.pixltone.images;
+
+import org.opencv.core.Mat;
+
+public class ImageLib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mat m;
+	}
+
+}
